d9e6f899368e4b1d8ccd5f8e55b079f5a5cfdc48
hootre <zxv7295@naver.com>
2023-05-30 01:11:17 +0900

    docs : nextjs13v setting docs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  experimental: { appDir: true },
   images: {
     domains: ['firebasestorage.googleapis.com', 'jubora.co.kr'],
   },
